c21490272c5584f7cf961c6d0b828e67da312642
Ashutosh Naik <ashutosh@pop-os.localdomain>
2023-11-10 08:58:28 -0700

    added main.py

---
+++ multi_agent_talecraft_llm/main.py
@@ -0,0 +1,22 @@
+
+
+def main():
+    # parse prompt param using arg parse
+
+    # build the gpt configuration object
+
+    # build the function map
+
+    # create our terminate msg function
+
+    # create a set of agents with specific roles
+        # admin user proxy agent - takes in the prompt and manages the group chat
+        # story architect agent - implements the overarching narrative logic and ensures story progression
+        # environment description agent - implements the environment description logic and ensures dynamic scene description generation
+        # character agents - 3 character agents each with their own personality and dialogue logic
+
+    # create a group chat and initiate the chat.
+
+
+if __name__ == '__main__':
+    main()
